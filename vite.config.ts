69ba57cf53de664a6c0156b2f301632375451770
itzik034 <itzik.king034@gmail.com>
2026-02-16 14:26:12 +0200

    settings update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/fullstack_course_crypto/',
     server: { open: true },
     build: {
         commonjsOptions: {
